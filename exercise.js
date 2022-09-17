60e6b92fbfa824ead3c987b74a7dedf5b3646f9e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 22:02:25 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,19 @@
+// If you run this function, you'll get an error. Fix the function and explain briefly what's wrong.
+// function canPlay() {
+//   let sport = " Football";
+//   if (true) {
+//     let personName = "Cosimo";
+//   }
+//   console.log(personName + sport);
+// }
+// canPlay();
+
+function canPlay() {
+  let sport = " Football";
+  let personName = null; // we need to declare the variable outside of the block scope so it can live outside the if block
+  if (true) {
+    personName = "Cosimo";
+  }
+  console.log(personName + sport);
+}
+canPlay();
